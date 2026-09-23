e98e195d987e6f46ce30cdf5d1ddd3b5760be570
Ashupari1983 <ashupari123@gmail.com>
2026-09-23 19:35:06 +0530

    acp

--- brackets.py
+++ brackets.py
@@ -1,11 +1,13 @@
 def curly(n, l=0, r=0):
     if l==n and r==n:
-        return 1
-    total = 0
+        return ['']
+    total = []
     if l>r:
-        total += curly(n, l, r+1)
+        for i in curly(n, l, r+1):
+            total.append('}' + i)
     if l<n:
-        total += curly(n, l+1, r)
+        for i in curly(n, l+1, r):
+            total.append('{' + i)
     return total
 
 a = int(input('Enter the number of brackets: '))
